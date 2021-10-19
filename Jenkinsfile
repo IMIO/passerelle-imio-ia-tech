e9ce388ed94de176da764894d4124e784149c2ad
Nicolas Hislaire <nicolas.hislaire@imio.be>
2021-10-19 12:14:32 +0200

    [TELE-1144] set install path in jenkins params

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,4 @@
 iateleservicesCreateDeb (
   packageName: "passerelle-imio-ia-tech",
+  installPath: "/usr/lib/python3/dist-packages",
 )
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = "1.0.7"
+version = "1.0.8"
 
 setup(
     name='passerelle-imio-ia-tech',
