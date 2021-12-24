06d174854c9eb0eed8bf2dff3aa8429f19705a32
nselva <nicolas.selva@imio.be>
2021-12-24 14:02:37 +0100

    [MTELECHAA68]add connecteur for location rooms et materials

--- passerelle_imio_ia_tech/models.py
+++ passerelle_imio_ia_tech/models.py
@@ -4,14 +4,31 @@ from io import BytesIO
 import requests
 from django.db import models
 from django.http import JsonResponse
-from django.utils.six.moves.urllib_parse import urljoin
+# from django.utils.six.moves.urllib_parse import urljoin
 from passerelle.base.models import BaseResource
 from passerelle.compat import json_loads
 from passerelle.utils.api import endpoint
 from passerelle.utils.jsonresponse import APIError
 
+import datetime
+import time
+import json
+import ast
+
 
 # TODO : we should rename this class name with something like AtalConnector
+def string_to_datetime(date_string):
+    """
+    convertit les dates string de Atal en datetime
+    :param date_string: string d'une date au format 2000-12-31T23:59...
+    :return: datetime(year, month, day, hour, minute)
+    """
+    # string de formatage pour strptime
+    format_datetime = "%Y-%m-%dT%H:%M"
+
+    return datetime.datetime.strptime(date_string[:16], format_datetime)
+
+
 class imio_atal(BaseResource):
     base_url = models.URLField(
         max_length=256,
@@ -189,7 +206,7 @@ class imio_atal(BaseResource):
             # and set in the passerelle connector settings.
             "X-API-Key": self.api_key,
         }
-        response = self.requests.get(url, headers=headers, verify=False,)
+        response = self.requests.get(url, headers=headers, verify=False, )
 
         # TODO : create an utilitary method to handle status_code >= 400
         if response.status_code >= 400:
@@ -232,3 +249,455 @@ class imio_atal(BaseResource):
         )
 
         return {"data": response.json()}  # must return dict
+
+    @endpoint(
+        name="get-room-loans",
+        perm="can_access",
+        description="Cherche les locations de salles dans ATAL.",
+        methods=["get"],
+    )
+    def read_reservations_room(self, request):
+        url = f"{self.base_url}/api/RoomLoans"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-reservation-room-detail",
+        perm="can_access",
+        description="Cherche les détails des réservations de salles dans ATAL.",
+        methods=["get"],
+    )
+    def read_reservations_room_details(self, request):
+        url = f"{self.base_url}/api/RoomLoans/Lines"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-reservation-room",
+        perm="can_access",
+        description="Lis une réservation de salle dans ATAL.",
+        methods=["get"],
+        parameters={
+            "id": {
+                "description": "id de la réservation",
+                "type": "int",
+                "example_value": "2",
+            },
+        },
+    )
+    def read_reservation_room(self, request, id):
+        url = f"{self.base_url}/api/RoomLoans/{id}"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-materiel-loans",
+        perm="can_access",
+        description="Cherche les locations de materiel dans ATAL.",
+        methods=["get"],
+    )
+    def read_reservations_materiel(self, request):
+        url = f"{self.base_url}/api/MaterialLoans"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-materiel-loans-details",
+        perm="can_access",
+        description="Cherche les détails des locations de materiel dans ATAL.",
+        methods=["get"],
+    )
+    def read_materiel_loans_details(self, request):
+        url = f"{self.base_url}/api/MaterialLoans/Lines"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-materiel-list",
+        perm="can_access",
+        description="Cherche le materiel louable dans ATAL.",
+        methods=["get"],
+    )
+    def read_materiel_list(self, request):
+        url = f"{self.base_url}/api/Patrimony"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        # pour avoir le patrimoine louable
+        params = {
+            "$filter": "Loanable",
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+                params=params,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        # retourne tout le patrimoine louable sauf les salles
+        return {"data": [x for x in response if "Type" in x and x["Type"] != 1]}  # must return dict
+
+    @endpoint(
+        name="get-reservation-materiel",
+        perm="can_access",
+        description="Lis une réservation de materiel dans ATAL.",
+        methods=["get"],
+        parameters={
+            "id": {
+                "description": "id de la réservation",
+                "type": "int",
+                "example_value": "2",
+            },
+        },
+    )
+    def read_reservation_materiel(self, request, id):
+        url = f"{self.base_url}/api/MaterialLoans/{id}"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-patrimoines-louable",
+        perm="can_access",
+        description="Cherche le patrimoine louable dans ATAL.",
+        methods=["get"],
+    )
+    def read_patrimoines_louable(self, request):
+        url = f"{self.base_url}/api/Patrimony"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+        }
+
+        params = {
+            "$filter": "Loanable",
+        }
+
+        try:
+            response = self.requests.get(
+                url,
+                headers=headers,
+                params=params,
+            ).json()
+        except Exception as e:
+            raise APIError(
+                str(e),
+                http_status=405,
+            )
+
+        return response  # must return dict
+
+    @endpoint(
+        name="get-rooms-name",
+        perm="can_access",
+        description="Cherche les noms des salles louables dans ATAL.",
+        methods=["get"],
+    )
+    def read_rooms_name(self, request):
+        # liste de tous le patrimoine louable
+        patrimoines = self.read_patrimoines_louable(request)
+
+        # tri en fonction du type 1 (salle)
+        return {"data": [x for x in patrimoines if "Type" in x and x["Type"] == 1]}  # must return dict
+
+    @endpoint(
+        name="get-rooms-dispo",
+        perm="can_access",
+        description="Cherche les salles disponible pour une date donnée dans ATAL.",
+        methods=["get"],
+        parameters={
+            "date_debut": {
+                "description": "date de debut de la location",
+                "type": "datetime.date",
+                "example_value": "2008-11-25",
+            },
+            "date_fin": {
+                "description": "date de fin de la location",
+                "type": "datetime.date",
+                "example_value": "2008-11-25",
+            },
+            "heure_debut": {
+                "description": "date de fin de la location",
+                "type": "datetime.time",
+                "example_value": "11:15",
+            },
+            "heure_fin": {
+                "description": "date de fin de la location",
+                "type": "datetime.time",
+                "example_value": "20:20",
+            },
+        },
+    )
+    def read_rooms_dispo(self, request, date_debut, date_fin, heure_debut, heure_fin):
+        # format date
+        date_debut = datetime.date(int(date_debut.split("-")[0]), int(date_debut.split("-")[1]),
+                                   int(date_debut.split("-")[2]))
+        date_fin = datetime.date(int(date_fin.split("-")[0]), int(date_fin.split("-")[1]), int(date_fin.split("-")[2]))
+        # format heure
+        heure_debut = time.strptime(heure_debut, "%H:%M")
+        heure_debut = datetime.time(heure_debut.tm_hour, heure_debut.tm_min)
+        heure_fin = time.strptime(heure_fin, "%H:%M")
+        heure_fin = datetime.time(heure_fin.tm_hour, heure_fin.tm_min)
+
+        # datetime debut demande location
+        datetime_debut = datetime.datetime.combine(date_debut, heure_debut)
+        # datetime fin demande location
+        datetime_fin = datetime.datetime.combine(date_fin, heure_fin)
+
+        # liste des locations
+        liste_location = self.read_reservations_room_details(request)
+
+        # init liste rooms non dispo
+        room_non_dispo = []
+
+        # parcours des locations
+        for location in liste_location:
+            # transformation des dates de location en datetime
+            debut_location = string_to_datetime(location["StartDate"])
+            fin_location = string_to_datetime(location["EndDate"])
+
+            # tri des salles en fonction des dates de locations
+            if debut_location < datetime_debut < fin_location or debut_location < datetime_fin < fin_location:
+                room_non_dispo.append(location["RoomId"])
+
+        # liste des salles
+        rooms = self.read_rooms_name(request)["data"]
+
+        # tri des salles pour avoir les dispo
+        return {"data": [x for x in rooms if x["Id"] not in room_non_dispo]}  # must return dict
+
+    @endpoint(
+        name="get-dates-dispo",
+        perm="can_access",
+        description="Cherche les dates disponible pour une salle dans ATAL.",
+        methods=["get"],
+        parameters={
+            "room": {
+                "description": "salle",
+                "type": "int",
+                "example_value": 2732,
+            },
+            "delai": {
+                "description": "délai minimum à l'introduction de la demande",
+                "type": "int",
+                "example_value": 90
+            },
+        },
+    )
+    def read_dates_dispo(self, request, room, delai=0):
+        # liste des locations
+        locations = self.read_reservations_room_details(request)
+
+        # définition de la date d'aujourd'hui en datetime
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+
+        # convert string to dict
+        # room = ast.literal_eval(room)
+
+        # tri des locations par rapport à une salle
+        locations = [x for x in locations if "RoomId" in x and x["RoomId"] == int(room) and (
+                    today + datetime.timedelta(days=delai)) < string_to_datetime(x["StartDate"])]
+
+        return {"data": locations}
+
+    @endpoint(
+        name="post-reservation-room",
+        perm="can_access",
+        description="Inscrit une réservation de salle dans Atal.",
+        methods=["get"],
+        parameters={
+            "date_debut": {
+                "description": "date de debut de la location",
+                "type": "datetime.date",
+                "example_value": "2022-12-01",
+            },
+            "date_fin": {
+                "description": "date de fin de la location",
+                "type": "datetime.date",
+                "example_value": "2022-12-02",
+            },
+            "heure_debut": {
+                "description": "date de fin de la location",
+                "type": "datetime.time",
+                "example_value": "11:15",
+            },
+            "heure_fin": {
+                "description": "date de fin de la location",
+                "type": "datetime.time",
+                "example_value": "20:20",
+            },
+            "room": {
+                "description": "salle",
+                "type": "int",
+                "example_value": 2732,
+            },
+            "nombre_personne_prevue": {
+                "description": "Nombre de personne prévue",
+                "type": "int",
+                "example_value": 0,
+            },
+            "nombre_personne_reel": {
+                "description": "Nombre de personne réel",
+                "type": "int",
+                "example_value": 0,
+            },
+            "id_tier": {
+                "description": "Aucune idée",
+                "type": "int",
+                "example_value": 63,
+            },
+        }
+    )
+    def write_reservation_room(self, request, date_debut, date_fin, heure_debut, heure_fin, room, nombre_personne_prevue=0, nombre_personne_reel=0, id_tier=63):
+        url = f"{self.base_url}/api/RoomLoans"
+        headers = {
+            "accept": "application/json",
+            # X-API-KEY is visible in ATAL admin panel
+            # and set in the passerelle connector settings.
+            "X-API-Key": self.api_key,
+            "Content-Type": "application/json",
+        }
+
+        datetime_debut = f"{date_debut}T{heure_debut}"
+        datetime_fin = f"{date_fin}T{heure_fin}"
+        # room = ast.literal_eval(room)
+
+        payload = json.dumps({
+            "EndDate": datetime_fin,
+            "PlannedPeopleNumber": nombre_personne_prevue,
+            "RealPeopleNumber": nombre_personne_reel,
+            "RequesterThirdPartyId": id_tier,
+            "Rooms": [
+                {
+                    "EndDate": datetime_fin,
+                    "Pricing": {
+                        "AdditionalQuantity": 0,
+                        "AppliedPricingId": 0,
+                        "BaseQuantity": 0,
+                        "SquareMetersNumber": 0
+                    },
+                    "RoomId": room,
+                    "StartDate": datetime_debut
+                }
+            ],
+            "StartDate": datetime_debut
+        })
+
+        response = self.requests.post(url, headers=headers, data=payload)
+
+        return response.json()
+
